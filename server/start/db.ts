db93ba49f467669b0a47da09a565f29aafc22466
Kai Lam <kai.chun.lam@gmail.com>
2025-12-29 22:58:49 +0800

    archiveDb func for development

--- server/start/db.ts
+++ server/start/db.ts
@@ -1,12 +1,16 @@
 import { v7 } from 'uuid'
 import Database from 'better-sqlite3'
 import { join } from 'node:path'
+import fs from 'node:fs'
 import q, { SqlQuery } from '../lib/query'
 import catchUniqErr from '../lib/catchUniqErr'
 import migrate from '../lib/migrate'
 
+const { NODE_ENV } = process.env
 const { dirname } = import.meta
-const dbPath = join(dirname, '../../volumes/db/app.db')
+const dbDir = join(dirname, '../../volumes/db')
+const dbPath = join(dbDir, 'app.db')
+const archiveDir = join(dbDir, 'archive')
 
 export class Db extends Database {
   query(query: SqlQuery) {
@@ -21,7 +25,18 @@ export function uuid() {
   return v7()
 }
 
+export function archiveDb() {
+  if (!fs.existsSync(dbPath)) return
+  if (!fs.existsSync(archiveDir)) {
+    fs.mkdirSync(archiveDir)
+  }
+  const ts = new Date().toISOString()
+  fs.renameSync(dbPath, join(archiveDir, `${ts}.db`))
+}
+
 function connect() {
+  if (NODE_ENV !== 'production') archiveDb()
+
   const db = new Db(dbPath)
   db.pragma('journal_mode = WAL')
   migrate(db)
