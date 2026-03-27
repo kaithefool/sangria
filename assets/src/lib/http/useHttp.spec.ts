5a847688df20b8bedf22110da8855dee1e3efa57
Kai Lam <kai.chun.lam@gmail.com>
2026-03-27 11:55:14 +0800

    testing lib for react

---
+++ assets/src/lib/http/useHttp.spec.ts
@@ -0,0 +1,4 @@
+import { describe, it, expect } from 'vitest'
+import { renderHook } from '@testing-library/react'
+
+describe('useHttp', () => {})
---
+++ assets/src/lib/http/useHttp.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { http, HttpPromise, HttpState, RequestConfig } from './http'
+
+export default function useHttp<T>(propConfig?: RequestConfig) {
+  const [promise, setPromise] = useState<HttpPromise<T>>()
+  const [state, setState] = useState<HttpState<T>>({ status: 'ready' })
+  const [fetched, setFetched] = useState<T>()
+  const [localConfig, setLocalConfig] = useState<RequestConfig>()
+  const config = localConfig ?? propConfig
+
+  const request = (r: RequestConfig) => setLocalConfig(r)
+  const abort = () => promise?.abort()
+  const refresh = () => {}
+
+  useEffect(() => {
+    let p: HttpPromise<T>
+    if (config) {
+      p = http<T>(config, (s) => {
+        setState(s)
+        if (s.status === 'success') setFetched(s.payload)
+      })
+      setPromise(p)
+    }
+
+    return () => {
+      p?.abort()
+    }
+  }, [config])
+
+  return {
+    promise,
+    ...state,
+    fetched,
+    config,
+    request,
+    abort,
+  }
+}
